96e8beebcbaed7b43ffd225e091182f97ca3a497
Nicolas Martyanoff <khaelin@gmail.com>
2019-10-27 16:15:59 +0100

    generate a minimal erlang module

--- generator/generator.go
+++ generator/generator.go
@@ -11,12 +11,18 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 package generator
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os"
+	"path"
+	"text/template"
 
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
 )
 
@@ -25,6 +31,16 @@ type Generator struct {
 	Response *plugin.CodeGeneratorResponse
 
 	Verbose bool
+
+	InputFileDescriptors []*descriptor.FileDescriptorProto
+
+	PackageName      string
+	PackageDirectory string
+
+	ErlModuleName string
+	ErlModulePath string
+
+	erlModuleTemplate *template.Template
 }
 
 func NewGenerator(req *plugin.CodeGeneratorRequest) (*Generator, error) {
@@ -34,6 +50,13 @@ func NewGenerator(req *plugin.CodeGeneratorRequest) (*Generator, error) {
 
 		Verbose: true,
 	}
+
+	erlModuleTemplate, err := ErlModuleTemplate()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"cannot create erlang module template: %w", err)
+	}
+	g.erlModuleTemplate = erlModuleTemplate
 
 	return &g, nil
 }
@@ -47,6 +70,100 @@ func (g *Generator) Info(format string, args ...interface{}) {
 }
 
 func (g *Generator) GenerateOutput() error {
-	// TODO
+	if err := g.collectData(); err != nil {
+		return err
+	}
+
+	g.ErlModuleName = g.ProtoPackageNameToErlModuleName(g.PackageName)
+	g.ErlModulePath = path.Join(g.PackageDirectory, g.ErlModuleName+".erl")
+
+	err := g.generateFile(g.ErlModulePath, g.erlModuleTemplate, g)
+	if err != nil {
+		return fmt.Errorf("cannot generate erlang module: %w", err)
+	}
+
+	return nil
+}
+
+func (g *Generator) collectData() error {
+	fns := []func() error{
+		g.collectInputFileDescriptors,
+		g.collectPackageName,
+		g.collectPackageDirectory,
+	}
+
+	for _, fn := range fns {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (g *Generator) collectInputFileDescriptors() error {
+	var fds []*descriptor.FileDescriptorProto
+
+	for _, name := range g.Request.FileToGenerate {
+		for _, fd := range g.Request.ProtoFile {
+			if fd.GetName() == name {
+				fds = append(fds, fd)
+			}
+		}
+	}
+
+	g.InputFileDescriptors = fds
+
+	return nil
+}
+
+func (g *Generator) collectPackageName() error {
+	var name string
+
+	for _, fd := range g.InputFileDescriptors {
+		if name != "" && name != fd.GetPackage() {
+			return errors.New("cannot process multiple files " +
+				"with different proto packages")
+		}
+
+		name = fd.GetPackage()
+	}
+
+	g.PackageName = name
+	return nil
+}
+
+func (g *Generator) collectPackageDirectory() error {
+	var dir string
+
+	for _, fd := range g.InputFileDescriptors {
+		fdDir := path.Dir(fd.GetName())
+		if dir != "" && dir != fdDir {
+			return errors.New("cannot process multiple files " +
+				"from different directories")
+		}
+
+		dir = fdDir
+	}
+
+	g.PackageDirectory = dir
+	return nil
+}
+
+func (g *Generator) generateFile(fileName string, tpl *template.Template, data interface{}) error {
+	g.Info("generating %s", fileName)
+
+	var buf bytes.Buffer
+	if err := tpl.Execute(&buf, data); err != nil {
+		return fmt.Errorf("cannot execute template: %w", err)
+	}
+
+	content := buf.String()
+	file := plugin.CodeGeneratorResponse_File{
+		Name:    &fileName,
+		Content: &content,
+	}
+
+	g.Response.File = append(g.Response.File, &file)
 	return nil
 }
